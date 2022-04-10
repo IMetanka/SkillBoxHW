16a35d3d21e3cbab84b21a574455df8838742c05
IMetanka <98908315+IMetanka@users.noreply.github.com>
2022-04-10 19:10:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число:");
+            int Number = int.Parse(Console.ReadLine());
+
+            if (Number % 2 == 0)
+            {
+                Console.WriteLine($"Четное число");
+            }
+            else
+            {
+                Console.WriteLine($"Нечетное число");
+            }
+
+        }
+    }
+}
